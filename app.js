8d5733ff334be40b56b8f53b864f7889997f808e
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2018-11-29 09:33:32 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+(function () {
+  function giphySearch(keyword) {
+    // Make a request to giphy API here
+  }
+
+  function appendImage(img) {
+    let $div = $('<div class="img-wrapper"></div>');
+    $('<div class="inner"></div>').append(img).appendTo($div);
+    $('#thumbs').append($div)
+  }
+
+  function showLoader() {
+    $('.loader-wrapper').addClass('shown');
+  }
+
+  function hideLoader() {
+    $('.loader-wrapper').removeClass('shown');
+  }
+
+  $('#searchForm').submit(async (ev) => {
+    ev.preventDefault();
+    let $input = $('#searchInput');
+  });
+})();
